b918eaf4e009ae960de386e647a863a5294b566e
Lucas Vidal <lucasvidal0904@gmail.com>
2023-05-07 16:44:32 -0300

    modificacion para CORS

--- src/main/java/com/portfolio/lucasvidal/Controller/UserController.java
+++ src/main/java/com/portfolio/lucasvidal/Controller/UserController.java
@@ -8,6 +8,7 @@ import com.portfolio.lucasvidal.Entity.Users;
 import com.portfolio.lucasvidal.Interface.IUserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lucas
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class UserController {
     @Autowired IUserService iUserService;
 
